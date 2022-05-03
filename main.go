5953c34461488445fc14c58fdfa6e34505731a32
dezhizhang <1541609448@qq.com>
2022-05-03 16:33:18 +0800

    feat:单向队列

--- main.go
+++ main.go
@@ -1,49 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
-type Node struct {
-	row int
-	col int
-	val int
+type Queue struct {
+	maxSize int
+	array   [4]int
+	front   int // 指向队列首位
+	rear    int // 表示指向队列的尾部
 }
 
-func main()  {
-	var chessMap [11][11]int
-	chessMap[1][2] = 1
-	chessMap[2][3] = 2
+//添加数据到队列中
 
-	var slice []Node
+func (this *Queue) AddQueue(val int) (err error) {
+	if this.rear == this.maxSize-1 {
+		return errors.New("队列以满")
+	}
+	this.rear++
+	this.array[this.rear] = val
+	return
+}
 
-	for i,v := range chessMap {
-		for j,v2 := range v {
-			if v2 != 0 {
-				node := Node{
-					row: i,
-					col: j,
-					val: v2,
-				}
-				slice = append(slice,node)
+// 队列中取数据
+
+func (this *Queue) GetQueue()(val int,err error) {
+	//先判断队列是否为空
+	if this.rear == this.front {
+		return -1,errors.New("队列为空")
+	}
+	this.front++
+	val = this.array[this.front]
+	return val,nil
+}
+
+// 显示数据
+
+func (this *Queue) ShowQueue() {
+	for i := this.front + 1; i <= this.rear; i++ {
+		fmt.Printf("array[%d]=%d\t", i, this.array[i])
+	}
+}
+
+func main() {
+
+	queue := &Queue{
+		maxSize: 5,
+		front: -1,
+		rear: -1,
+	}
+
+	var key string
+	var val int
+
+	for {
+		fmt.Println("1.输入add表示添加数据到队列中")
+		fmt.Println("2.输入get表示从队列中获取数据")
+		fmt.Println("3.输入show表示显示队列")
+		fmt.Println("4.输入exit表示退出")
+
+	 	fmt.Scanln(&key)
+		switch key {
+		case "add":
+			fmt.Println("输入你要入队列数")
+			fmt.Scanln(&val)
+			err := queue.AddQueue(val)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
 			}
+			fmt.Println("加入队列成功")
+		case "get":
+			value,err := queue.GetQueue()
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			fmt.Printf("value=%v",value)
+		case "show":
+			queue.ShowQueue()
+		case "exit":
+			os.Exit(0)
 		}
 	}
-
-	for _,val := range slice {
-		fmt.Printf("row=%v,col=%v,val=%v",val.row,val.col,val.val)
-		fmt.Println()
-	}
-
-	//恢复原始数据
-	var chessMap2[11][11]int
-	for _,node := range slice {
-		chessMap2[node.row][node.col] = node.val
-	}
-
-	for _,v := range chessMap2 {
-		for _,v2 := range v {
-			fmt.Printf("%v\t",v2)
-		}
-		fmt.Println()
-	}
-
 }
