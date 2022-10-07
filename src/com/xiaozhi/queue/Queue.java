7bfec4780c824d1aacdafe615627c2bb7a210aa3
dezhizhang <1541609448@qq.com>
2022-10-07 12:26:08 +0800

    feat:update

--- src/com/xiaozhi/queue/Queue.java
+++ src/com/xiaozhi/queue/Queue.java
@@ -53,10 +53,10 @@ public class Queue {
   }
 }
 
-class ArrayQueue {
+class ArrayQueue{
   private int maxSize; //表示数组的最大容量
   private int front; //队列头
-  private int rear; // 队表尾
+  private int rear; //队表尾
   private int arr[]; //用于存放数据
 
   public ArrayQueue(int maxSize) {
@@ -65,50 +65,45 @@ class ArrayQueue {
     this.front = -1;
     this.rear = -1;
   }
-
   // 判断队列是满
   public boolean isFull() {
     return rear == maxSize - 1;
   }
-
   // 判断队列是否为空
   public boolean isEmpty() {
-    return rear == front - 1;
+    return rear == front;
   }
-
-  // 添加数据到队列中
+  //添加数据到队列中
   public void addQueue(int n) {
-    if (isFull()) {
-      System.out.println("队列已满，不能加入数据");
+    if(isEmpty()) {
+      System.out.printf("队列已满，不能加入数据");
       return;
     }
     this.rear++;
-    this.arr[rear] = n;
+    this.arr[this.rear] = n;
   }
 
   // 获取队列的数据
   public int getQueue() {
-    if (isEmpty()) {
+    if(isEmpty()) {
       throw new RuntimeException("队列为空不能取数据");
     }
     this.front++;
     return this.arr[this.front];
   }
-
   // 显示队列的所有数据
   public void showQueue() {
-    if (isEmpty()) {
+    if(isEmpty()) {
       System.out.println("队表为空");
       return;
     }
-    for (int i = 0; i < arr.length; i++) {
+    for (int i=0;i < arr.length;i++) {
       System.out.printf("arr[%d]=%d\n", i, arr[i]);
     }
   }
-
   // 获取列表头数据
-  public int headQueue() {
-    if (isEmpty()) {
+  public int headQueue(){
+    if(isEmpty()) {
       throw new RuntimeException("队列为空");
     }
     return this.arr[this.front + 1];
