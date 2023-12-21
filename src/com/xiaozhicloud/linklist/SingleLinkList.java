ec0e75c412ffd0ce03167a24a74be81fd4d1b8ad
dezhizhang <1541609448@qq.com>
2023-12-21 21:40:27 +0800

    feat: 单链表

---
+++ src/com/xiaozhicloud/linklist/SingleLinkList.java
@@ -0,0 +1,31 @@
+package src.com.xiaozhicloud.linklist;
+
+public class SingleLinkList {
+  public static void main(String[] args) {
+
+  }
+}
+
+
+class HeroNode {
+  public int no;
+  public String name;
+  public String nickname;
+  public HeroNode next;
+
+  public HeroNode(int no, String name,String nickname) {
+    this.no = no;
+    this.name = name;
+    this.nickname = nickname;
+  }
+
+  @Override
+  public String toString() {
+    return "HeroNode{" +
+      "no=" + no +
+      ", name='" + name + '\'' +
+      ", nickname='" + nickname + '\'' +
+      ", next=" + next +
+      '}';
+  }
+}
