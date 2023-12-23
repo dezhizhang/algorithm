c16284511edb20e34f7882b7b5d5945891ddb2f7
dezhizhang <1541609448@qq.com>
2023-12-23 15:22:12 +0800

    feat: 链表的修改

--- src/com/xiaozhicloud/linklist/SingleLinkList.java
+++ src/com/xiaozhicloud/linklist/SingleLinkList.java
@@ -6,11 +6,18 @@ class SingleLinkListDemo {
     HeroNode hero2 = new HeroNode(2,"卢俊义","玉諆房");
     HeroNode hero3 = new HeroNode(3,"呈用","智多星");
 
+
+    HeroNode newHero2 =  new HeroNode(2,"卢俊义1","玉諆房1");
+
     SingleLinkList singleLinkList = new SingleLinkList();
     singleLinkList.addByOrder(hero1);
     singleLinkList.addByOrder(hero3);
-    singleLinkList.addByOrder(hero3);
     singleLinkList.addByOrder(hero2);
+
+
+    singleLinkList.update(newHero2);
+
+
     singleLinkList.list();
 
   }
@@ -30,9 +37,10 @@ class SingleLinkList{
     temp.next = heroNode;
   }
   // 按顺序加入节点
-  public void addByOrder(HeroNode heroNode) {
+  public void  addByOrder(HeroNode heroNode) {
     HeroNode temp = head;
     boolean flag = false;
+
     while (true) {
       if(temp.next == null) {
         break;
@@ -40,18 +48,54 @@ class SingleLinkList{
       if(temp.next.no > heroNode.no) {
         break;
       }
-      if (temp.next.no == heroNode.no) {
+      if(temp.next.no == heroNode.no) {
         flag = true;
         break;
       }
       temp = temp.next;
     }
+
     if(flag) {
       System.out.printf("准备插入的英雄的编号%d 已存在不能添加",heroNode.no);
+      System.out.println();
       return;
     }
+
     heroNode.next = temp.next;
     temp.next = heroNode;
+
+  }
+  // 修改链表
+  public void update(HeroNode newHeroNode) {
+    if(head.next == null) {
+      System.out.println("链表为空~");
+      return;
+    }
+    HeroNode temp = head.next;
+    boolean flag = false;
+    while (true) {
+      if(temp == null) {
+        break; // 已经遍历完链表
+      }
+      if(temp.no == newHeroNode.no) {
+        flag = true;
+        break;
+      }
+
+      temp = temp.next;
+
+    }
+
+    // 根据flag判断是否找到要修改节点
+    if(flag) {
+      temp.name = newHeroNode.name;
+      temp.nickname = newHeroNode.nickname;
+    }
+
+    if(!flag) {
+      System.out.printf("没有找到要修改的节点%d",newHeroNode.no);
+    }
+
   }
   // 显示链表
   public void list() {
