9f1c122c097fe994881f85130b99bbd781263998
dezhizhang <1541609448@qq.com>
2023-12-24 13:18:08 +0800

    feat: 双向链表

--- src/com/xiaozhicloud/linklist/DoubleLinkList.java
+++ src/com/xiaozhicloud/linklist/DoubleLinkList.java
@@ -2,12 +2,32 @@ package src.com.xiaozhicloud.linklist;
 
 class DoubleLinkListTest {
   public static void main(String[] args) {
+    HeroNode2 heroNode2 = new HeroNode2(1, "宋江", "及时丽");
+    HeroNode2 heroNode21 = new HeroNode2(2, "卢俊义", "玉妄谈");
+    HeroNode2 heroNode22 = new HeroNode2(3, "吴用", "智多星");
+
+    DoubleLinkList doubleLinkList = new DoubleLinkList();
+    doubleLinkList.add(heroNode2);
+    doubleLinkList.add(heroNode21);
+    doubleLinkList.add(heroNode22);
+
+    doubleLinkList.list();
+
+
+    System.out.println("---------------");
+
+    HeroNode2 heroNode23 = new HeroNode2(2, "公孙胜", "入云龙");
+    doubleLinkList.update(heroNode23);
+
+    doubleLinkList.delete(2);
+
+    doubleLinkList.list();
 
   }
 }
 
-class DoubleLinkList{
-  private HeroNode2 head = new HeroNode2(0,"","");
+class DoubleLinkList {
+  private HeroNode2 head = new HeroNode2(0, "", "");
 
   // 获取链表的头节点
   public HeroNode2 getHead() {
@@ -18,7 +38,7 @@ class DoubleLinkList{
   public void add(HeroNode2 heroNode2) {
     HeroNode2 temp = head;
     while (true) {
-      if(temp.next == null) {
+      if (temp.next == null) {
         break;
       }
       temp = temp.next;
@@ -28,33 +48,33 @@ class DoubleLinkList{
   }
 
   // 双向链表修改
-  public void  update(HeroNode2 newHeroNode2) {
-    if(head.next == null) {
+  public void update(HeroNode2 newHeroNode2) {
+    if (head.next == null) {
       System.out.println("链表为空");
       return;
     }
     HeroNode2 temp = head.next;
     boolean flag = false;
     while (true) {
-      if(temp == null) {
+      if (temp == null) {
         break;
       }
-      if(temp.no == newHeroNode2.no) {
+      if (temp.no == newHeroNode2.no) {
         flag = true;
         break;
       }
       temp = temp.next;
     }
-    if(flag) {
+    if (flag) {
       temp.name = newHeroNode2.name;
       temp.nickname = newHeroNode2.nickname;
       return;
     }
-    System.out.printf("没有找到%d的节点",newHeroNode2.no);
+    System.out.printf("没有找到%d的节点", newHeroNode2.no);
   }
 
   public void delete(int no) {
-    if(head.next == null) {
+    if (head.next == null) {
       System.out.println("链表为空，不能删除");
       return;
     }
@@ -62,23 +82,23 @@ class DoubleLinkList{
     HeroNode2 temp = head.next;
     boolean flag = false;
     while (true) {
-      if(temp == null) {
+      if (temp == null) {
         break;
       }
-      if(temp.no == no) {
+      if (temp.no == no) {
         flag = true;
         break;
       }
       temp = temp.next;
     }
-    if(flag) {
+    if (flag) {
       temp.prev.next = temp.next;
-      if(temp.next != null) {
+      if (temp.next != null) {
         temp.next.prev = temp.prev;
       }
       return;
     }
-    System.out.printf("要删除的节点不存在%d",no);
+    System.out.printf("要删除的节点不存在%d", no);
 
   }
 
@@ -91,7 +111,7 @@ class DoubleLinkList{
     }
     HeroNode2 temp = head.next;
     while (true) {
-      if(temp == null) {
+      if (temp == null) {
         break;
       }
       System.out.println(temp);
@@ -108,7 +128,7 @@ class HeroNode2 {
   public HeroNode2 next;
   public HeroNode2 prev; // 指向前一个位置的节点
 
-  public HeroNode2(int no, String name,String nickname) {
+  public HeroNode2(int no, String name, String nickname) {
     this.no = no;
     this.name = name;
     this.nickname = nickname;
@@ -116,10 +136,6 @@ class HeroNode2 {
 
   @Override
   public String toString() {
-    return "HeroNode{" +
-      "no=" + no +
-      ", name='" + name + '\'' +
-      ", nickname='" + nickname +
-      '}';
+    return "HeroNode{" + "no=" + no + ", name='" + name + '\'' + ", nickname='" + nickname + '}';
   }
 }
