5915b9c8c4746a628b787742c34e2339f9824204
dezhizhang <1541609448@qq.com>
2023-12-27 06:06:48 +0800

    feat: 八皇后

--- src/com/xiaozhicloud/recursion/Queue8.java
+++ src/com/xiaozhicloud/recursion/Queue8.java
@@ -3,8 +3,25 @@ package src.com.xiaozhicloud.recursion;
 public class Queue8 {
   int max = 8;
   int[] array = new int[max];
+  static int count = 0;
   public static void main(String[] args) {
+      Queue8 queue8 = new Queue8();
+      queue8.check(0);
+      System.out.println(count);
+  }
 
+  public void check(int n) {
+    if(n == max) {
+      print();
+      return;
+    }
+
+    for(int i=0;i < max;i++) {
+      array[n] = i;
+      if(judge(n)) {
+        check(n + 1);
+      }
+    }
   }
 
   private boolean judge(int n) {
@@ -17,6 +34,7 @@ public class Queue8 {
   }
 
   private void print() {
+    count++;
     for(int i=0;i < array.length;i++) {
       System.out.print(array[i] + " ");
     }
