60d9e966c0126de2035c4bb046fcd374a95b747e
dezhizhang <1541609448@qq.com>
2023-12-27 07:13:25 +0800

    feat:冒泡排序优化

--- src/com/xiaozhicloud/sort/BubbleSort.java
+++ src/com/xiaozhicloud/sort/BubbleSort.java
@@ -4,20 +4,28 @@ import java.util.Arrays;
 
 public class BubbleSort {
   public static void main(String[] args) {
-      int arr[] = {3,9,-1,10,-2};
-
-      int temp = 0;
-      for(int i=0;i < arr.length - 1;i++) {
-        for(int j=0;j < arr.length - 1 - i;j++) {
-          if(arr[j] > arr[j + 1]) {
-            temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-          }
+    int arr[] = {3,9,-1,10,20};
+    int temp = 0;
+    boolean flag = false;
+    for(int i=0;i < arr.length - 1;i++) {
+      flag = false;
+      for(int j=0;j < arr.length - 1;j++) {
+        if(arr[j] > arr[j + 1]) {
+          flag = true;
+          temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
         }
       }
+      System.out.printf("第(%d)趟排序",i + 1);
+      System.out.println();
+      if(!flag) {
+        break;
+      }
 
-      System.out.println(Arrays.toString(arr));
+    }
+
+    System.out.println(Arrays.toString(arr));
   }
 
 }
