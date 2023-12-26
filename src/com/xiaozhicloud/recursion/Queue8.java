054536da346d4116262885eb4f63bc8ca94fa014
dezhizhang <1541609448@qq.com>
2023-12-26 23:19:37 +0800

    feat: 添加输入方法

---
+++ src/com/xiaozhicloud/recursion/Queue8.java
@@ -0,0 +1,17 @@
+package src.com.xiaozhicloud.recursion;
+
+public class Queue8 {
+  int max = 8;
+  int[] array = new int[max];
+  public static void main(String[] args) {
+
+  }
+
+  private void print() {
+    for(int i=0;i < array.length;i++) {
+      System.out.print(array[i] + " ");
+    }
+    System.out.println();
+  }
+
+}
