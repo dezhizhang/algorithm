e74595647386b48125e1afc0eb95905017b2904c
dezhizhang <1541609448@qq.com>
2023-12-26 23:28:22 +0800

    feat: 判断方法

--- src/com/xiaozhicloud/recursion/Queue8.java
+++ src/com/xiaozhicloud/recursion/Queue8.java
@@ -7,6 +7,15 @@ public class Queue8 {
 
   }
 
+  private boolean judge(int n) {
+    for(int i = 0;i < n;i++) {
+      if(array[i] == array[n] || Math.abs(n - i) == Math.abs(array[n] - array[i])) {
+        return  false;
+      }
+    }
+    return true;
+  }
+
   private void print() {
     for(int i=0;i < array.length;i++) {
       System.out.print(array[i] + " ");
