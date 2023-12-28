c1c7d6acadc7c5a7c1b8a8b143a1831d168310e4
dezhizhang <1541609448@qq.com>
2023-12-28 19:46:16 +0800

    feat: 选择排序

---
+++ src/com/xiaozhicloud/sort/SelectSort.java
@@ -0,0 +1,34 @@
+package src.com.xiaozhicloud.sort;
+
+import java.util.Arrays;
+
+public class SelectSort {
+  public static void main(String[] args) {
+    int[] arr = {101, 34, 199, 1};
+    selectSort(arr);
+
+
+  }
+
+  public static void selectSort(int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      int minIndex = i;
+      int min = arr[minIndex];
+      for (int j = i + 1; j < arr.length; j++) {
+        if (min > arr[j]) {
+          min = arr[j];
+          minIndex = j;
+        }
+      }
+      if (minIndex != i) {
+        arr[minIndex] = arr[i];
+        arr[i] = min;
+
+      }
+    }
+
+    System.out.println(Arrays.toString(arr));
+
+  }
+
+}
