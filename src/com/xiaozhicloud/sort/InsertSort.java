2be733ccc21a584ba928112c1044e6aaa2125bfe
dezhizhang <1541609448@qq.com>
2023-12-28 20:27:21 +0800

    feat: 插入排序

---
+++ src/com/xiaozhicloud/sort/InsertSort.java
@@ -0,0 +1,23 @@
+package src.com.xiaozhicloud.sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+  public static void main(String[] args) {
+    int[] arr = {101, 34, 119, 1,-1,89};
+    insertSort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+
+  public static void insertSort(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      int insertVal = arr[i];
+      int insetIndex = i - 1;
+      while (insetIndex >= 0 && insertVal < arr[insetIndex]) {
+        arr[insetIndex + 1] = arr[insetIndex];
+        insetIndex--;
+      }
+      arr[insetIndex + 1] = insertVal;
+    }
+  }
+}
--- src/com/xiaozhicloud/sort/SelectSort.java
+++ src/com/xiaozhicloud/sort/SelectSort.java
@@ -4,31 +4,26 @@ import java.util.Arrays;
 
 public class SelectSort {
   public static void main(String[] args) {
-    int[] arr = {101, 34, 199, 1};
+    int[] arr = {101, 34, 199, 1,90,123};
     selectSort(arr);
-
-
   }
 
-  public static void selectSort(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
+  public static void selectSort(int[]arr) {
+    for(int i=0;i < arr.length - 1;i++) {
       int minIndex = i;
       int min = arr[minIndex];
-      for (int j = i + 1; j < arr.length; j++) {
-        if (min > arr[j]) {
-          min = arr[j];
+      for(int j=i + 1;j < arr.length;j++) {
+        if(min > arr[j]) {
+          min = arr[i];
           minIndex = j;
         }
       }
-      if (minIndex != i) {
+      if(minIndex != i) {
         arr[minIndex] = arr[i];
         arr[i] = min;
-
       }
     }
-
     System.out.println(Arrays.toString(arr));
-
   }
 
 }
