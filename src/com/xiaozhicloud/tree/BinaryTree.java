ba050205845d718b7347a22aa4d4f9dc308f4887
dezhizhang <1541609448@qq.com>
2023-12-31 10:53:42 +0800

    feat: 二叉树的删除

--- src/com/xiaozhicloud/tree/BinaryTree.java
+++ src/com/xiaozhicloud/tree/BinaryTree.java
@@ -230,6 +230,29 @@ class HeroNode {
     return curNode;
   }
 
+  // 删除节点
+  public void deleteNode(int no) {
+    if(this.left != null && this.left.no == no) {
+      this.left = null;
+      return;
+    }
+
+    if(this.right != null && this.right.no == no) {
+      this.right = null;
+      return;
+    }
+
+    if(this.left != null) {
+      this.left.deleteNode(no);
+
+    }
+
+    if(this.right != null) {
+      this.right.deleteNode(no);
+    }
+
+  }
+
   @Override
   public String toString() {
     return "HeroNode[no=" + no + ",name=" + name + "]";
