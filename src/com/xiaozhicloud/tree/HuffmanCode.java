266f95ffff5fe97debe50c74c0ed733a6a456807
dezhizhang <1541609448@qq.com>
2023-12-31 23:39:37 +0800

    feat:赫夫曼编码

--- src/com/xiaozhicloud/tree/HuffmanCode.java
+++ src/com/xiaozhicloud/tree/HuffmanCode.java
@@ -8,7 +8,8 @@ public class HuffmanCode {
     byte[] contentBytes = content.getBytes();
 
     List<NodeCode> nodes = getNodes(contentBytes);
-    System.out.println(nodes);
+    NodeCode huffmanTree = createHuffmanTree(nodes);
+    preOrder(huffmanTree);
   }
 
   public static List<NodeCode> getNodes(byte[] bytes) {
@@ -32,8 +33,37 @@ public class HuffmanCode {
   }
 
   // 通过list创建对应的赫夫曼树
-  private static Node createHuffmanTree(List<NodeCode> nodes) {
+  private static NodeCode createHuffmanTree(List<NodeCode> nodes) {
+    while (nodes.size() > 1) {
+      Collections.sort(nodes);
 
+      // 取出第一颗最小的二叉树
+      NodeCode leftNode = nodes.get(0);
+      // 取出第二颗最小的二叉树
+      NodeCode rightNode = nodes.get(1);
+
+      NodeCode parent = new NodeCode(null,leftNode.weight + rightNode.weight);
+
+      parent.left = leftNode;
+      parent.right = rightNode;
+
+      nodes.remove(leftNode);
+      nodes.remove(rightNode);
+
+      // 将新的二叉树加入到nodes
+      nodes.add(parent);
+
+    }
+    return nodes.get(0);
+  }
+
+  // 前序遍历方法
+  private static void preOrder(NodeCode root) {
+    if(root == null) {
+      System.out.println("树为空");
+      return;
+    }
+    root.preOrder();
   }
 
 }
@@ -60,4 +90,13 @@ class NodeCode implements Comparable<NodeCode> {
     return "Node[data=" + data + "weight=" + weight + "]";
   }
   // 前序遍历
+  public void preOrder() {
+    System.out.println(this);
+    if(this.left != null) {
+      this.left.preOrder();
+    }
+    if(this.right != null) {
+      this.right.preOrder();
+    }
+  }
 }
