3285c14c9aa03c35cb147c7abac4820927c7a40d
dezhizhang <1541609448@qq.com>
2024-01-04 22:31:17 +0800

    feat: 广度优先遍历

--- src/com/xiaozhicloud/graph/Graph.java
+++ src/com/xiaozhicloud/graph/Graph.java
@@ -2,6 +2,7 @@ package src.com.xiaozhicloud.graph;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 
 class GraphTest{
@@ -19,6 +20,8 @@ class GraphTest{
     graph.insertEdge(1,3,1);
     graph.insertEdge(1,4,1);
     graph.showGraph();
+
+    graph.dfs();
   }
 }
 
@@ -27,11 +30,82 @@ public class Graph {
   private ArrayList<String> vertexList; // 存储顶点的集合
   private int[][] edges;
   private int numOfEdges; // 表示边的数目
+  private boolean[] isVisited;
   public Graph(int n) {
     edges = new int[n][n];
+    isVisited = new boolean[n];
     vertexList = new ArrayList<String>(n);
     numOfEdges = 0;
   }
+  // 得到第一个领接节点的下标w
+  public int getFirstNeighbor(int index) {
+    for(int j=0;j < vertexList.size();j++) {
+      if(edges[index][j] > 0) {
+        return j;
+      }
+    }
+    return  -1;
+  }
+
+  // 根据前一个领接节点的下标返回下一个领接节点
+  private int getNextNeighbor(int v1,int v2) {
+    for(int j=v2 + 1;j < vertexList.size();j++) {
+      if(edges[v1][j] > 0) {
+        return j;
+      }
+    }
+    return -1;
+  }
+
+  // 深度优先遍历算法
+  public void dfs(boolean[] isVisited,int i) {
+    System.out.print(getValueByIndex(i) + "->");
+
+    // 将节点设置为已经访问
+    isVisited[i] = true;
+
+    int w = getFirstNeighbor(i);
+
+    while (w !=-1) {
+      if(!isVisited[w]) {
+        dfs(isVisited,w);
+      }
+      w = getNextNeighbor(i,w);
+    }
+  }
+
+  public void dfs() {
+    for(int i=0;i < getNumOfVertex();i++) {
+      if(!isVisited[i]) {
+        dfs(isVisited,i);
+      }
+    }
+  }
+
+  private void bfs(boolean[] isVisited,int i) {
+    int u; //表示队列的头结点对应下标
+    int w; //邻接结点
+
+    LinkedList queue = new LinkedList();
+    System.out.println(getValueByIndex(i) + "=>");
+    isVisited[i] = true;
+    queue.addLast(i);
+    while (!queue.isEmpty()) {
+     u =(Integer)queue.removeFirst();
+     w = getFirstNeighbor(u);
+     while (w !=-1) {
+       if(!isVisited[w]) {
+         System.out.println(getValueByIndex(i) + "=>");
+         isVisited[w] = true;
+         // 入队
+         queue.addLast(w);
+       }
+       w = getNextNeighbor(u,w);
+     }
+    }
+  }
+
+
 
   // 插入顶点
   public void insertVertex(String vertex) {
