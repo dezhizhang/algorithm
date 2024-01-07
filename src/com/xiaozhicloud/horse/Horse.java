8b0a9031671e2a2294f986e80c1ca45529045beb
dezhizhang <1541609448@qq.com>
2024-01-08 07:02:13 +0800

    feat:骑土周游问题解决

--- src/com/xiaozhicloud/horse/Horse.java
+++ src/com/xiaozhicloud/horse/Horse.java
@@ -8,7 +8,31 @@ public class Horse {
   private static int X; // 棋盘的列数
   private static int Y; // 棋盘的行数
 
+  private static boolean visited[]; // 标记是否被访问过
+  private static boolean finished; // 如果为true表示成功
+
   public static void main(String[] args) {
+
+  }
+
+  public static void traversalChessboard(int[][] chessboard, int row, int colum, int step) {
+    chessboard[row][colum] = step;
+    visited[row * X + colum] = true; // 标记已访问
+    ArrayList<Point> ps = next(new Point(colum, row)); // 获取可以走的下一步
+    while (!ps.isEmpty()) {
+      Point p = ps.remove(0); // 取出下一个可以走的位置
+      // 判断是否访问过
+      if (!visited[p.y * X + p.x]) {
+        traversalChessboard(chessboard, p.y, p.x, step + 1);
+      }
+    }
+
+    if (step < X * Y && !finished) {
+      chessboard[row][colum] = 0;
+      visited[row * X + colum] = false;
+    } else {
+      finished = true;
+    }
 
   }
 
@@ -43,14 +67,13 @@ public class Horse {
       ps.add(new Point(p1));
     }
     // 判断马儿可以走3位置
-    if ((p1.x = curPoint.x - 1) >=0 && (p1.y = curPoint.y + 2) < Y) {
+    if ((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y + 2) < Y) {
       ps.add(new Point(p1));
     }
     // 判断马儿可以走4位置
-    if ((p1.x = curPoint.x - 2) >=0 && (p1.y = curPoint.y + 1) < Y) {
+    if ((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y + 1) < Y) {
       ps.add(new Point(p1));
     }
-
 
     return ps;
   }
